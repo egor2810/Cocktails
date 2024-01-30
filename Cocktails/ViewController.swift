0509e302fa8826d7379573abb57d9c3ac43a8cbc
Ablogin Egor <47031969+egor2810@users.noreply.github.com>
2024-01-30 17:58:35 +0300

    Initial Commit

---
+++ Cocktails/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Cocktails
+//
+//  Created by Егор Аблогин on 30.01.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
